ac909ff161df0496175e0dfeae0b4e65c292e2a9
Vakso Andrey <nate20@mail.ru>
2023-04-15 16:31:40 +0300

    feat: add solution for some tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+
+  const numString = n.toString();
+
+  let maxNum = parseInt(numString[0]);
+
+  for (let i = 0; i < numString.length; i++) {
+    // remove the current digit from the string
+    const withoutDigit = numString.slice(0, i) + numString.slice(i + 1);
+
+    const numWithoutDigit = parseInt(withoutDigit);
+    if (numWithoutDigit > maxNum) {
+      maxNum = numWithoutDigit;
+    }
+  }
+  return maxNum;
 }
 
 module.exports = {
--- src/encode-line.js
+++ src/encode-line.js
@@ -11,31 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function encodeLine(str) {
-  let strArr = str.split('');
-  let result;
-  let resObj = {};
-  let obj = {};
-  
-  strArr.forEach(function(key) {
-      if(!(key in resObj)) {
-          resObj[key] = [key];
-      } else if(key in resObj) {
-          resObj[key].push(key);
-      }
-  })
-  for(let key in resObj) {
-    obj[key] = resObj[key].length;
+  let result = '';
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i+1]) {
+      count++;
+    } else {
+      result += (count > 1 ? count : '') + str[i];
+      count = 1;
+    }
+  }
+
+  return result;
 }
-
-  const strobj = []
-
-Object.entries(obj).forEach(([key, value]) => {
-  strobj.push([value, key])
-})
-  let strResult = Object.entries(strobj).join('').split(',').join('')
-  return strResult
-}
-
 
 module.exports = {
   encodeLine
